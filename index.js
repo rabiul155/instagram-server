117343e85a9916afb5553a0e27262b6d09ad4099
rabiul155 <siyamict@gmail.com>
2023-04-06 16:05:22 +0600

    post add

--- index.js
+++ index.js
@@ -2,9 +2,85 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT | 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i4cqwjk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+
+        const usersCollection = client.db("Instagram").collection("users")
+        const postsCollection = client.db("Instagram").collection("posts")
+
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/posts', async (req, res) => {
+            const query = {}
+            const result = await postsCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    finally {
+
+
+
+    }
+
+
+}
+run().catch(console.dir);
+
+
+
+
 
 
 app.get('/', (req, res) => {
